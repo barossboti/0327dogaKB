53c5ae76ef66e1334b314f36b29a77de13a9e3dc
barossboti <150018760+barossboti@users.noreply.github.com>
2025-03-27 10:56:20 +0100

    Add files via upload

---
+++ Adatsor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eutazas
+{
+    internal class Adatsor
+    {
+        public int sorszam { get; set; }
+        public string datum { get; set; }
+        public int azonosito { get; set; }
+        public string jegy { get; set; }
+        public int ervenyesseg { get; set; }
+        public Adatsor(string sor)
+        {
+            string[] sz = sor.Split(' ');
+            sorszam=Convert.ToInt32(sz[0]);
+            datum= sz[1];
+            azonosito = Convert.ToInt32(sz[2]);
+            jegy = sz[3];
+            ervenyesseg = Convert.ToInt32(sz[4]);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace eutazas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] f = File.ReadAllLines("utasadat.txt");
+            List<Adatsor> adatok = new List<Adatsor>();
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                adatok.Add(new Adatsor(f[i]));
+            }
+            Console.WriteLine("2. feladat");
+            Console.WriteLine($"A buszra {adatok.Count} utas akart felszállni.");
+
+
+            Console.WriteLine("3. feladat");
+            int db = 0;
+            foreach (var i in adatok)
+            {
+                if (i.ervenyesseg.ToString() == "JGY")
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"A buszra {db} utas nem szállhatott fel ");
+
+
+            Console.WriteLine("4. feladat");
+            var legtobbfelszallo = adatok.GroupBy(x => x.sorszam).OrderByDescending(x => x.Count()).ToList();
+            Console.WriteLine($"A legtöbb utas {legtobbfelszallo[0].Count()} fő a 8. megállóban próbált felszállni.");
+
+            Console.WriteLine("5. feladat");
+            var ingyen = adatok.Where(x => x.jegy == "NYP" || x.jegy == "RVS" || x.jegy == "GYK" ).Count();
+            var kedvezmeny = adatok.Where(x => x.jegy == "TAB" || x.jegy == "NYB").Count();
+
+            Console.WriteLine($"Ingyenesen utazók száma: {ingyen}");
+            Console.WriteLine($"Kedvezményesen utazok száma: {kedvezmeny}");
+            Console.ReadKey();
+        }
+    }
+}
